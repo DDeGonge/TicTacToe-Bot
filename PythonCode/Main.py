ee932917fb35ebb228b131b3441d7d43b3eb6678
DDeGonge888 <DDeGonge888@gmail.com>
2020-09-05 18:43:38 -0400

    cleanup for release

--- PythonCode/Main.py
+++ PythonCode/Main.py
@@ -22,50 +22,21 @@ def main():
         cam.start_camera()
 
         while True:
-            button_input = True
-            if button_input:
-                bot_first = random.choice([True, False])
-                game_function = 'standard'  # random.choice(GAMETYPES)
+            _ = input('Press enter to start game...')
 
-                if game_function == 'standard':
-                    standard_game(scarabot, cam, spkr, bot_first = bot_first)
-                elif game_function == 'meme':
-                    meme_game(scarabot, cam, spkr, bot_first = bot_first)
+            # Future improvements can happen here
+            bot_first = True  # random.choice([True, False])
+            game_function = 'standard'  # random.choice(GAMETYPES)
+
+            # Start game
+            if game_function == 'standard':
+                standard_game(scarabot, cam, spkr, bot_first = bot_first)
+            elif game_function == 'meme':
+                meme_game(scarabot, cam, spkr, bot_first = bot_first)
                 
     except Exception as e:
         raise e
 
-def draw_butterfly():
-    sd = SerialDevice()
-    scara = Scara(sd)
-    scara.user_zero()
-    scara.absolute_move(20,20)
-    scara.send_gcode('butter.g')
-
-def test():
-    sd = SerialDevice()
-    scara = Scara(sd)
-    cam = Camera()
-    cam.start_camera()
-    spkr = Speaker()
-    
-    scara.user_zero()
-    # scara.draw_board()
-
-    # testmove = Move(xindex=0, yindex=0)
-    # scara.draw_move(testmove)
-    # testmove = Move(xindex=1, yindex=1)
-    # scara.draw_move(testmove)
-    # testmove = Move(xindex=0, yindex=2)
-    # scara.draw_move(testmove)
-
-    standard_game(scara, cam, spkr, bot_first = True)
-
-    scara.absolute_move(0, 0)
-
-    scara.disable()
 
 if __name__=='__main__':
-    # main()
-    test()
-    # draw_butterfly()
+    main()
